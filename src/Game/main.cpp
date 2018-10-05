5239cff07d452fc5d828e8b08fcba83c2939109c
Kyle Chart (s4907469) <s4907469@bournemouth.ac.uk>
2018-10-05 10:54:07 +0100

    Updated main.cpp

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,8 +1,26 @@
+#include <HoneyComb/HoneyComb.h>
 #include <iostream>
+#include <memory>
+
+class Player : public Component
+{
+public:
+	void onTick()
+	{
+		std::cout << "Hello World" << std::endl;
+	}
+};
+
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
 
+	std::shared_ptr<Core> core = std::make_shared<Core>();
+	std::shared_ptr<Entity> player = core->addEntity();
+	//player->addCompnent<Player>();
+
+	core->start();
+
+	
 	return 0;
 }
